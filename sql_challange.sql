1f80dbaf0718904bb7c332326838ee6aec6bf157
unknown <robertodiazbriones@gmail.com>
2021-02-02 13:27:44 -0500

    All completed

--- sql_challange.sql
+++ sql_challange.sql
@@ -64,78 +64,21 @@ SELECT * FROM dept_manager
 SELECT e.emp_no, e.last_name, e.first_name,	e.sex, s.salary 
 FROM employees e
 JOIN salaries s
-ON (e.emp_no=s.emp_no)
+ON (e.emp_no=s.emp_no);
 
 --2.List first name, last name, and hire date for employees who were hired in 1986
 
-SELECT e.emp_no, e.first_name, e.last_name, e.hire_date 
+SELECT e.first_name, e.last_name, e.hire_date 
 FROM employees e
-WHERE (e.hire_date >= '1986-01-01' AND e.hire_date <= '1986-12-31')
+WHERE (e.hire_date >= '1986-01-01' AND e.hire_date <= '1986-12-31');
 
 --3.List the manager of each department with the following information: department number, department name,
 --the manager's employee number, last name, first name.
-SELECT first_name, last_name
-FROM employees
-WHERE emp_no
-IN (
-   SELECT emp_no
-    FROM dept_man
-    WHERE dept_no
-    IN (
-        SELECT dept_no
-        FROM departments
-        WHERE dept_name
-        IN (
-              SELECT title_id
-              FROM titles
-              WHERE emp_title_id = 'Manager'
-            )
-        )
-  );
-
-
-SELECT dept_name
-FROM departments
-WHERE dept_no
-IN (
-   SELECT dept_no 
-    FROM dept_manager
-    WHERE emp_no
-    IN (
-        SELECT first_name     
-		FROM employees 
-        WHERE emp_title_id
-        IN (
-              SELECT title_id
-              FROM titles
-              WHERE title = 'Manager'
-            )
-        )
-  )
-
-SELECT dept_no 
-FROM dept_manager
-WHERE emp_no
-IN (
-	SELECT emp_no, first_name, last_name     
-	FROM employees
-	WHERE emp_no
-	IN (
-		SELECT 
-    	
-	)
-
-
-
-
-SELECT emp_no,first_name, last_name      
-FROM employees
-WHERE emp_title_id
-     IN (
-              SELECT title_id
-              FROM titles
-              WHERE emp_title_id = 'Manager'
-		 
+SELECT dm.dept_no, d.dept_name, e.emp_no, e.last_name,e.first_name  
+FROM dept_manager AS dm
+INNER JOIN departments AS d ON (dm.dept_no = d.dept_no)
+INNER JOIN employees AS e ON (dm.emp_no = e.emp_no);
+ 
 --4 List the department of each employee with the following information: 
 --employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name,e.first_name, d.dept_name   
@@ -149,39 +92,21 @@ INNER JOIN (SELECT dept_no, dept_name
 --"Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex       
 FROM employees as e
-WHERE e.first_name='Hercules' AND e.last_name LIKE '%B%'
+WHERE e.first_name='Hercules' AND e.last_name LIKE '%B%';
 		 
 --6 List all employees in the Sales department, 
 --including their employee number, last name, first name, and department name.
-SELECT e.emp_no, e.last_name, e.first_name  
+SELECT e.emp_no, e.last_name, e.first_name, de.emp_no, d.dept_name
 FROM employees AS e 
-WHERE emp_no
-IN (
-    SELECT emp_no
-    FROM dept_emp
-    WHERE dept_no 
-    IN (
-		SELECT dept_no
-		FROM departments
-		WHERE dept_name='Sales'
-		)
-	)
-		 
+INNER JOIN dept_emp AS de ON (de.emp_no = e.emp_no)
+INNER JOIN departments AS d ON (d.dept_no = de.dept_no) AND (d.dept_name='Sales');
+
 --7. List all employees in the Sales and Development departments,
 --including their employee number, last name, first name, and department name.
-SELECT e.emp_no, e.last_name, e.first_name  
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e 
-WHERE emp_no
-IN (
-    SELECT emp_no
-    FROM dept_emp
-    WHERE dept_no 
-    IN (
-		SELECT dept_no
-		FROM departments
-		WHERE dept_name='Sales' OR dept_name='Development'
-		)
-	)
+INNER JOIN dept_emp AS de ON (de.emp_no = e.emp_no)
+INNER JOIN departments AS d ON (d.dept_no = de.dept_no) AND (d.dept_name='Sales') OR (d.dept_name='Development')
 
 --8 In descending order, list the frequency count of employee last names, 
 --i.e., how many employees share each last name.
